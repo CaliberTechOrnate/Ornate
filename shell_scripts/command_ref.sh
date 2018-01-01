2dce6f55e8b26b107b5233b243a7f91c41530406
Mano <mano.net@gmail.com>
2018-01-01 09:24:23 -0700

    some shell/terminal commands to get the job done easily

---
+++ shell_scripts/command_ref.sh
@@ -0,0 +1,16 @@
+#install an installer to suit your operating system, such as brew for mac
+
+#install node package manager globally 
+npm install -g protractor
+
+#refer this page for details http://www.protractortest.org/#/
+
+#check version of npm just to be sure
+npm --version
+
+#Git commands
+#Cloning a branch
+git clone -b <branch> <remote_repo>
+
+
+
